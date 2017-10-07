75920948f9a2d25487789e8bd69fee17bb0e9788
drklrd <srvbhattarai@gmail.com>
2017-10-07 15:21:26 +0545

    add card view

--- app/src/main/java/com/example/drklrd/photaharu/MainActivity.java
+++ app/src/main/java/com/example/drklrd/photaharu/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -12,12 +14,18 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView photaHaruList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        photaHaruList = (RecyclerView) findViewById(R.id.photoHaruList);
+        photaHaruList.setHasFixedSize(true);
+        photaHaruList.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
