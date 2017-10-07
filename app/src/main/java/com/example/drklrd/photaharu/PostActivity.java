1026a2e683c12afc1e66f7ba52968323b4a5fe62
drklrd <srvbhattarai@gmail.com>
2017-10-07 14:27:53 +0545

    add image picker

--- app/src/main/java/com/example/drklrd/photaharu/PostActivity.java
+++ app/src/main/java/com/example/drklrd/photaharu/PostActivity.java
@@ -1,13 +1,38 @@
 package com.example.drklrd.photaharu;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class PostActivity extends AppCompatActivity {
+
+    private static final int GALLERY_REQUEST =222;
+    private Uri uri = null;
+    private ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+    }
+
+    public void pickImage(View view){
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("Image/*");
+        startActivityForResult(galleryIntent,GALLERY_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 222 && resultCode == RESULT_OK){
+            uri = data.getData();
+            imageButton = (ImageButton) findViewById(R.id.selectedImage);
+            imageButton.setImageURI(uri);
+        }
     }
 }
