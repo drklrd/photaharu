0fb9abd39a9a9718b6d30ff7c65beead24dce34b
drklrd <srvbhattarai@gmail.com>
2017-10-07 20:59:20 +0545

    add uid and username to the post

--- app/src/main/java/com/example/drklrd/photaharu/PostActivity.java
+++ app/src/main/java/com/example/drklrd/photaharu/PostActivity.java
@@ -2,6 +2,7 @@ package com.example.drklrd.photaharu;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -11,9 +12,16 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -28,6 +36,9 @@ public class PostActivity extends AppCompatActivity {
     private StorageReference storageReference;
     private FirebaseDatabase database;
     private DatabaseReference databaseReference;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabaseUsers;
+    private FirebaseUser mCurrentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +48,10 @@ public class PostActivity extends AppCompatActivity {
         imageDescription = (EditText) findViewById(R.id.imageDescription);
         storageReference = FirebaseStorage.getInstance().getReference();
         databaseReference = database.getInstance().getReference().child("Photaharu");
+
+        mAuth = FirebaseAuth.getInstance();
+        mCurrentUser = mAuth.getCurrentUser();
+        mDatabaseUsers = FirebaseDatabase.getInstance().getReference().child("Users").child(mCurrentUser.getUid());
     }
 
     public void pickImage(View view){
@@ -67,21 +82,62 @@ public class PostActivity extends AppCompatActivity {
                 filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                     @Override
                     public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        final Uri downloadUrl = taskSnapshot.getDownloadUrl();
                         Toast.makeText(PostActivity.this,"Successfully uploaded !",Toast.LENGTH_LONG).show();
-                        DatabaseReference newPost = databaseReference.push();
-                        newPost.child("title").setValue(title);
-                        newPost.child("description").setValue(description);
-                        newPost.child("image").setValue(downloadUrl.toString());
+                        final DatabaseReference newPost = databaseReference.push();
+
+                        mDatabaseUsers.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                newPost.child("title").setValue(title);
+                                newPost.child("description").setValue(description);
+                                newPost.child("image").setValue(downloadUrl.toString());
+                                newPost.child("uid").setValue(mCurrentUser.getUid());
+                                newPost.child("username").setValue(dataSnapshot.child("name").getValue()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if(task.isSuccessful()){
+                                            Intent mainActivityIntent = new Intent(PostActivity.this,MainActivity.class);
+                                            startActivity(mainActivityIntent);
+                                        }
+                                    }
+                                });
+                            }
+
+                            @Override
+                            public void onCancelled(DatabaseError databaseError) {
+
+                            }
+                        });
+
+
                     }
                 });
             }else{
                 Log.i("HERE***********","NICEEEEEE");
-                DatabaseReference newPost = databaseReference.push();
-                newPost.child("title").setValue(title);
-                newPost.child("description").setValue(description);
-                Log.i("HERE***********","NICEEEEEE222");
-                Toast.makeText(PostActivity.this,"Successfully uploaded !",Toast.LENGTH_LONG).show();
+                final DatabaseReference newPost = databaseReference.push();
+                mDatabaseUsers.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        newPost.child("title").setValue(title);
+                        newPost.child("description").setValue(description);
+                        newPost.child("uid").setValue(mCurrentUser.getUid());
+                        newPost.child("username").setValue(dataSnapshot.child("name").getValue()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    Intent mainActivityIntent = new Intent(PostActivity.this,MainActivity.class);
+                                    startActivity(mainActivityIntent);
+                                }
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
             }
 
         }
