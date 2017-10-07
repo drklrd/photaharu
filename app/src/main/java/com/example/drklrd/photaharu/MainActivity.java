8776f0b8b371bc724fd43e5a0da2c7459e8a233d
drklrd <srvbhattarai@gmail.com>
2017-10-07 14:01:33 +0545

    add new post activity

--- app/src/main/java/com/example/drklrd/photaharu/MainActivity.java
+++ app/src/main/java/com/example/drklrd/photaharu/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.drklrd.photaharu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,15 +18,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -45,6 +37,10 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        }
+        if(id == R.id.addPhoto){
+            Intent intent = new Intent(MainActivity.this,PostActivity.class);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
